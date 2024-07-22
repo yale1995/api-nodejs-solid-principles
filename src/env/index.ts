f327a5bf39af0b478c8e435c25ed58d03c94fde9
Yale Araujo <yale850@gmail.com>
2024-07-22 18:27:21 -0300

    feat: first commit

---
+++ src/env/index.ts
@@ -0,0 +1,16 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['development', 'test', 'production']),
+  PORT: z.coerce.number(),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (!_env.success) {
+  console.error('❌ Invalid environment variables', _env.error.format())
+  throw new Error('Invalid environment variables.')
+}
+
+export const env = _env.data
