4de87faecb3f2e147596bcd7bdcaf3517e3b7b4a
Yale Araujo <yale850@gmail.com>
2024-07-23 12:48:55 -0300

    feat: repository pattern

---
+++ src/http/use-cases/register.ts
@@ -0,0 +1,27 @@
+import { hash } from 'bcryptjs'
+import { UsersRepository } from '../repositories/users-repository'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+
+interface RegisterUseCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+export class RegisterUseCase {
+  constructor(private usersRepository: UsersRepository) {
+    this.usersRepository = usersRepository
+  }
+
+  execute = async ({ email, name, password }: RegisterUseCaseRequest) => {
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
+
+    if (userWithSameEmail) {
+      throw new UserAlreadyExistsError()
+    }
+
+    await this.usersRepository.create({ email, name, password_hash })
+  }
+}
