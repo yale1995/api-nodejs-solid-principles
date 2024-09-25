6f79bcff8992fbb672609bfc02579efa95289af3
Yale Araujo <yale850@gmail.com>
2024-09-25 16:37:34 -0300

    feat: fetch user check ins history use case and tests

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -37,4 +37,10 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     this.items.push(checkIn)
     return checkIn
   }
+
+  async findManyByUserId(userId: string, page: number) {
+    return this.items
+      .filter((item) => item.user_id === userId)
+      .slice((page - 1) * 20, page * 20)
+  }
 }
