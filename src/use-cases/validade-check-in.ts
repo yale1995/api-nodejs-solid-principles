d7d60c45902c3fbd20059cdf168232b51c0a0d6e
Yale Araujo <yale850@gmail.com>
2024-09-27 08:58:56 -0300

    feat: validate check-in use case and tests

--- src/use-cases/validade-check-in.ts
+++ src/use-cases/validade-check-in.ts
@@ -1,6 +1,8 @@
 import { CheckInsRepository } from '@/repositories/check-ins-repository'
 import { CheckIn } from '@prisma/client'
 import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import dayjs from 'dayjs'
+import { LateCheckInValidationError } from './errors/late-check-in-validation-error'
 
 interface ValidateCheckInUseCaseRequest {
   checkInId: string
@@ -22,6 +24,15 @@ export class ValidadeCheckInUseCase {
 
     if (!checkIn) {
       throw new ResourceNotFoundError()
+    }
+
+    const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+      checkIn.created_at,
+      'minutes',
+    )
+
+    if (distanceInMinutesFromCheckInCreation > 20) {
+      throw new LateCheckInValidationError()
     }
 
     checkIn.validated_at = new Date()
--- src/use-cases/validate-check-in.spec.ts
+++ src/use-cases/validate-check-in.spec.ts
@@ -1,4 +1,4 @@
-import { expect, describe, it, beforeEach } from 'vitest'
+import { expect, describe, it, beforeEach, afterEach, vi } from 'vitest'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
 import { ValidadeCheckInUseCase } from './validade-check-in'
 import { ResourceNotFoundError } from './errors/resource-not-found-error'
@@ -11,12 +11,12 @@ describe('Validate CheckIn Use Case', () => {
     checkInsRepository = new InMemoryCheckInsRepository()
     sut = new ValidadeCheckInUseCase(checkInsRepository)
 
-    // vi.useFakeTimers()
+    vi.useFakeTimers()
   })
 
-  // afterEach(() => {
-  //   vi.useRealTimers()
-  // })
+  afterEach(() => {
+    vi.useRealTimers()
+  })
 
   it('should be able to validate the check-in', async () => {
     const createdCheckIn = await checkInsRepository.create({
@@ -36,5 +36,21 @@ describe('Validate CheckIn Use Case', () => {
     await expect(() =>
       sut.execute({ checkInId: 'inexistent-check-in-id' }),
     ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+
+  it('should not be able to validate the check-in after 20 minutes of its creation', async () => {
+    vi.setSystemTime(new Date(2023, 0, 1, 13, 40))
+
+    const createdCheckIn = await checkInsRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    })
+
+    const twentyOneMinutesInMs = 1000 * 60 * 21
+    vi.advanceTimersByTime(twentyOneMinutesInMs)
+
+    await expect(() =>
+      sut.execute({ checkInId: createdCheckIn.id }),
+    ).rejects.toBeInstanceOf(Error)
   })
 })
