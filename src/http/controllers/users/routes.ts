75cf08d18aff0f3904003a4726b71a1dbc02acac
Yale Araujo <yale850@gmail.com>
2024-10-10 17:18:39 -0300

    feat: refresh token and e2e tests

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,11 +3,13 @@ import { register } from './register'
 import { authenticate } from './authenticate'
 import { profile } from './profile'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
+import { refresh } from './refresh'
 
 export const usersRoutes = async (app: FastifyInstance) => {
   // ============================ unauthenticated routes
   app.post('/users', register)
   app.post('/sessions', authenticate)
+  app.patch('/token/refresh', refresh)
 
   // ============================ authenticated routes
   app.get('/me', { onRequest: [verifyJWT] }, profile)
